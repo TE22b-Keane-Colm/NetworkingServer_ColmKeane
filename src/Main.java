14eea6f6825ea5ac4d816deb921f35c24189bc9d
colm.keane <colm.keane@elev.ga.ntig.se>
2025-05-22 22:22:31 +0200

    klar

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,42 @@ public class Main {
         }
 
         //vänta på data från klienten.
+        while (true){
+            try{
+                //väntar på att en anslutning till socketet och acepterar den
+                // metoden blockerar tills en ansluting är gjord till socketen
+                System.out.println("Väntar på anslutnings förfrågan...");
+                klient = server.accept();
+
+                System.out.println("Anslutnings förfrågan är accepterad...");
+                String klientHost = klient.getInetAddress().getHostAddress();
+                int klientport = klient.getPort();
+                System.out.println("Klient host = " + klientHost + "Klient port = " + klientport);
+
+                //Läs in data
+                InputStream klientIn = klient.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(klientIn));
+                String medFrnKlient = br.readLine();
+                System.out.println("Meddelande mottaget från klienten: " + medFrnKlient);
+
+                //Skickar svar till klienten
+                if (medFrnKlient != null && ! medFrnKlient.equalsIgnoreCase("bye")) {
+                    OutputStream klientOut = klient.getOutputStream();
+                    PrintWriter pw = new PrintWriter(klientOut, true);
+                    String ansMsg = "Hello, " + medFrnKlient;
+                    pw.println(ansMsg);
+                }
+
+                //Stänga ner sockets
+                if (medFrnKlient != null && medFrnKlient.equalsIgnoreCase("bye")){
+                    server.close();
+                    klient.close();
+                    break;
+                }
+
+            } catch (IOException ie){
+                System.out.println("ERROR, Något gick FEL.");
+            }
+        }
     }
 }
